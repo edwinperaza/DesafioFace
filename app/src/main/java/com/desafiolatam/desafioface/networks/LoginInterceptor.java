dac03655f1348ee17813de0d2e48b157b9dcdb71
Edwin Peraza <edwinperaza87@gmail.com>
2018-09-07 21:31:28 -0300

    Agregar el interceptor de retrofit

---
+++ app/src/main/java/com/desafiolatam/desafioface/networks/LoginInterceptor.java
@@ -0,0 +1,20 @@
+package com.desafiolatam.desafioface.networks;
+
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class LoginInterceptor {
+
+    public static final String BASE_URL = "https://empieza.desafiolatam.com/";
+
+    public Session get() {
+        Retrofit interceptor = new Retrofit
+                .Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return interceptor.create(Session.class);
+    }
+}
