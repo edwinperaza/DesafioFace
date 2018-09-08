6a427b65c1620176928a5ab5aa41e4d505f23a53
Edwin Peraza <edwinperaza87@gmail.com>
2018-09-07 21:42:33 -0300

    Presenter y MVP Login

---
+++ app/src/main/java/com/desafiolatam/desafioface/views/login/SessionCallback.java
@@ -0,0 +1,9 @@
+package com.desafiolatam.desafioface.views.login;
+
+public interface SessionCallback {
+
+    void requiredField();
+    void mailFormat();
+    void success();
+    void failure();
+}
---
+++ app/src/main/java/com/desafiolatam/desafioface/views/login/Signin.java
@@ -0,0 +1,53 @@
+package com.desafiolatam.desafioface.views.login;
+
+import com.desafiolatam.desafioface.models.CurrentUser;
+import com.desafiolatam.desafioface.networks.LoginInterceptor;
+import com.desafiolatam.desafioface.networks.Session;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+//Presenter
+public class Signin {
+
+    private SessionCallback callback;
+
+    public Signin(SessionCallback callback) {
+        this.callback = callback;
+    }
+
+    public void toServer(String email, String password) {
+
+        if (email.trim().length() <= 0 || password.trim().length() <= 0) {
+            callback.requiredField();
+        } else {
+            if (!email.contains("@")) {
+                callback.mailFormat();
+            } else {
+                Session session = new LoginInterceptor().get();
+                Call<CurrentUser> call = session.login(email, password);
+
+                call.enqueue(new Callback<CurrentUser>() {
+                    @Override
+                    public void onResponse(Call<CurrentUser> call, Response<CurrentUser> response) {
+                        if (200 == response.code() && response.isSuccessful()) {
+                            CurrentUser user = response.body();
+                            user.create();
+                            callback.success();
+                        } else {
+                            callback.failure();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<CurrentUser> call, Throwable t) {
+                        callback.failure();
+                    }
+                });
+            }
+        }
+
+
+    }
+}
