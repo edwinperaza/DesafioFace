85a98a66435c21e74c3743fc96dcbea11a809ae6
Edwin Peraza <edwinperaza87@gmail.com>
2018-09-07 21:53:11 -0300

    Implementar Splash

--- app/src/main/java/com/desafiolatam/desafioface/views/splash/SplashActivity.java
+++ app/src/main/java/com/desafiolatam/desafioface/views/splash/SplashActivity.java
@@ -1,10 +1,13 @@
 package com.desafiolatam.desafioface.views.splash;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.desafiolatam.desafioface.R;
+import com.desafiolatam.desafioface.views.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity implements LoginCallback {
 
@@ -21,6 +24,7 @@ public class SplashActivity extends AppCompatActivity implements LoginCallback {
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        new LoginValidation(this).init();
     }
 
     @Override
@@ -30,6 +34,12 @@ public class SplashActivity extends AppCompatActivity implements LoginCallback {
 
     @Override
     public void signUp() {
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 1200);
     }
 }
